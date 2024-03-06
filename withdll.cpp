554019f18f9bc04e84ef012c7fab3768aa349e0b
Quentin Santos <qsantos@qsantos.fr>
2024-03-06 22:26:53 +0100

    Fix compilation issues when using MSYS2

--- withdll.cpp
+++ withdll.cpp
@@ -1,8 +1,8 @@
 //  Microsoft Research Detours Package
 //  Copyright (c) Microsoft Corporation.  All rights reserved.
+#include <windows.h>
 #include <stdio.h>
 #include <strsafe.h>
-#include <windows.h>
 
 #include <detours.h>
 
