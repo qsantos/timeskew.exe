9a8d1c30122966f468f3e67b29537b9d298e3410
Quentin Santos <qsantos@qsantos.fr>
2024-05-14 20:49:12 +0200

    Extract test_relative_time

--- tester.py
+++ tester.py
@@ -23,81 +23,46 @@ except FileNotFoundError:
     pass
 environ["TIMESKEW_LOGFILE"] = LOGFILE
 
+
+def test_relative_time(p: Popen, time_ratio: float) -> None:
+    sleep(.1)
+    p.stdin.write(b'\n')
+    p.stdin.flush()
+    start = time()
+    assert p.wait() == 0
+    real_elapsed = time() - start
+    fake_elapsed = float(p.stdout.read().decode())
+    print(f'{real_elapsed=}')
+    print(f'{fake_elapsed=}')
+    # Check whether the duration is consistent in real and skewed time
+    assert isclose(real_elapsed, SLEEP_DURATION / time_ratio, rel_tol=TOLERANCE)
+    assert isclose(fake_elapsed, SLEEP_DURATION, rel_tol=TOLERANCE)
+
+
 print('Test with no time acceleration')
 p = Popen(COMMAND, stdin=PIPE, stdout=PIPE)
-sleep(.1)
-p.stdin.write(b'\n')
-p.stdin.flush()
-start = time()
-assert p.wait() == 0
-real_elapsed = time() - start
-fake_elapsed = float(p.stdout.read().decode())
-print(f'{real_elapsed=}')
-print(f'{fake_elapsed=}')
-# It should still be pretty close to 1
-assert isclose(real_elapsed, SLEEP_DURATION, rel_tol=TOLERANCE)
-assert isclose(fake_elapsed, SLEEP_DURATION, rel_tol=TOLERANCE)
+test_relative_time(p, 1)
 
 print('Test time acceleration with environment-variables')
 p = Popen(COMMAND, stdin=PIPE, stdout=PIPE, env={"TIMESKEW": "10 1", **environ})
-sleep(.1)
-p.stdin.write(b'\n')
-p.stdin.flush()
-start = time()
-assert p.wait() == 0
-real_elapsed = time() - start
-fake_elapsed = float(p.stdout.read().decode())
-print(f'{real_elapsed=}')
-print(f'{fake_elapsed=}')
-assert isclose(real_elapsed, SLEEP_DURATION / 10, rel_tol=TOLERANCE)
-assert isclose(fake_elapsed, SLEEP_DURATION, rel_tol=TOLERANCE)
+test_relative_time(p, 10)
 
 print('Test time slow-down with environment-variables')
 p = Popen(COMMAND, stdin=PIPE, stdout=PIPE, env={"TIMESKEW": "1 10", **environ})
-sleep(.1)
-p.stdin.write(b'\n')
-p.stdin.flush()
-start = time()
-assert p.wait() == 0
-real_elapsed = time() - start
-fake_elapsed = float(p.stdout.read().decode())
-print(f'{real_elapsed=}')
-print(f'{fake_elapsed=}')
-assert isclose(real_elapsed , SLEEP_DURATION * 10, rel_tol=TOLERANCE)
-assert isclose(fake_elapsed, SLEEP_DURATION, rel_tol=TOLERANCE)
+test_relative_time(p, .1)
 
 print('Test time acceleration with port')
 p = Popen(COMMAND, stdin=PIPE, stdout=PIPE, env={"TIMESKEW_PORT": str(PORT), **environ})
 sleep(.1)
 with create_connection(('127.0.0.1', PORT)) as sock:
     sock.sendall(b"10 1\n")
-sleep(.1)
-p.stdin.write(b'\n')
-p.stdin.flush()
-start = time()
-assert p.wait() == 0
-real_elapsed = time() - start
-fake_elapsed = float(p.stdout.read().decode())
-print(f'{real_elapsed=}')
-print(f'{fake_elapsed=}')
-assert isclose(real_elapsed, SLEEP_DURATION / 10, rel_tol=TOLERANCE)
-assert isclose(fake_elapsed, SLEEP_DURATION, rel_tol=TOLERANCE)
+test_relative_time(p, 10)
 
 print('Test time slow-down with port')
 p = Popen(COMMAND, stdin=PIPE, stdout=PIPE, env={"TIMESKEW_PORT": str(PORT), **environ})
 sleep(.1)
 with create_connection(('127.0.0.1', PORT)) as sock:
     sock.sendall(b"1 10\n")
-sleep(.1)
-p.stdin.write(b'\n')
-p.stdin.flush()
-start = time()
-assert p.wait() == 0
-real_elapsed = time() - start
-fake_elapsed = float(p.stdout.read().decode())
-print(f'{real_elapsed=}')
-print(f'{fake_elapsed=}')
-assert isclose(real_elapsed, SLEEP_DURATION * 10, rel_tol=TOLERANCE)
-assert isclose(fake_elapsed, SLEEP_DURATION, rel_tol=TOLERANCE)
+test_relative_time(p, .1)
 
 assert filecmp.cmp(LOGFILE, EXPECTED)
